a4569e92a48cd6742b568bcc91f62d9d3c4afe8d
Gabe Leonard <gabe@databaseguy.com>
2022-03-16 02:45:26 -0400

    Can roll different kinds of dice
    Also better visuals

--- multi_roller.js
+++ multi_roller.js
@@ -1,23 +1,43 @@
 $(document).ready(function() {
 
-  // $("#rollsettemplate.removerow").click(remove_rollset);
-  // $("#rollsettemplate.addroll").click(addrolltorollset);
-  // $("#rollsets").on("click", ".removeroll", remove_rollset);
-
-
-  $("#add_rollset").click(function(e) {
-    let rollset_name_input = $("#add_rollset_name");
-    let newrollset_name = rollset_name_input.val();
-    rollset_name_input.val("");
-    _add_rollset(newrollset_name);
-  });
+  $("#add_rollset").click(add_rollset);
 
   let rollset = _add_rollset("Goblin Attacks");
   _add_roll_to_rollset(rollset, "Goblin Warrior", 10);
   _add_roll_to_rollset(rollset, "Goblin Wizard", -10);
+
+  let rollset2 = _add_rollset("Goblin Damage", 2, 8);
+  _add_roll_to_rollset(rollset2, "Goblin Warrior", 3);
+  _add_roll_to_rollset(rollset2, "Goblin Wizard", -2);
 });
 
-let _add_rollset = function(rollset_name) {
+let add_rollset = function(e) {
+  let rollset_name_input = $("#add_rollset_name");
+  let rollset_dice_input = $("#add_rollset_dice");
+  let rollset_sides_input = $("#add_rollset_sides");
+  let name = rollset_name_input.val();
+  let dice = rollset_dice_input.val();
+  let sides = rollset_sides_input.val();
+  rollset_name_input.val("");
+  rollset_dice_input.val("");
+  rollset_sides_input.val("");
+  if (!dice) {
+    dice = 1;
+  }
+  if (!sides) {
+    sides = 20;
+  }
+  _add_rollset(name, dice, sides);
+};
+
+
+var check_enter_rollset = function(event) {
+    if (event.keyCode == 13) {
+        add_rollset();
+    }
+}
+
+let _add_rollset = function(rollset_name, dice=1, sides=20) {
   let rollsets = $("#rollsets");
   let newrollset = $("#rollset_template").clone();
 
@@ -36,6 +56,10 @@ let _add_rollset = function(rollset_name) {
 
   let roll_results = $("#roll_result_template").clone();
   roll_results.removeAttr("id");
+  let dice_data = roll_results.find(".roll_data").first();
+  dice_data.text(`${dice}d${sides}`);
+  dice_data.data("dice", dice);
+  dice_data.data("sides", sides);
 
   let roll_button = $("#roll_button_template").clone();
   roll_button.removeAttr("id");
@@ -52,22 +76,43 @@ let _add_rollset = function(rollset_name) {
   $("#rollsets .add_roll").click(add_roll_to_rollset);
   $("#rollsets .roll_button").off("click");
   $("#rollsets .roll_button").click(multiroll);
+  $("#rollsets .roll_name").off("keypress");
+  $("#rollsets .roll_name").keypress(check_enter_roll);
+  $("#rollsets .roll_modifier").off("keypress");
+  $("#rollsets .roll_modifier").keypress(check_enter_roll);
 
   return newrollset;
+};
+
+var check_enter_roll = function(e) {
+  if (event.keyCode == 13) {
+      add_roll_to_rollset(e);
+  }
 }
 
 var multiroll = function(e) {
   let parent = $(event.target).parents(".rollset_rolls").first();
+
+  let dice_data = parent.find(".roll_data").first();
+  let dice = dice_data.data("dice");
+  let sides = dice_data.data("sides");
+
   let modifier_doms = parent.find(".roll .modifier");
   let modifiers = [];
   $.each(modifier_doms, function(key, value) {
     modifiers.push(Number($(value).text()));
   });
+
   let rolls = [];
   let high = null;
   let low = null;
+
   for (const modifier of modifiers) {
-    let roll = Math.ceil(Math.random() * 20) + modifier;
+    roll = 0;
+    for (let i = 0; i < dice; i++) {
+      roll += Math.ceil(Math.random() * sides);
+    }
+    roll += modifier;
     rolls.push(roll);
     if (high == null || roll > high) {
       high = roll;
@@ -76,7 +121,7 @@ var multiroll = function(e) {
       low = roll;
     }
   }
-  let result_dom = parent.find(".result_div .result");
+  let result_dom = parent.find(".result_div .result.value");
   let best_dom = parent.find(".roll_result_container .best").first();
   let worst_dom = parent.find(".roll_result_container .worst").first();
 
@@ -146,6 +191,11 @@ var save_data = function(e) {
     let rollset_data = [];
     let rollset = $(value);
     let rollset_name = rollset.find(".rollset_name").first().text();
+
+    let dice_data = rollset.find(".roll_data").first();
+    let dice = dice_data.data("dice");
+    let sides = dice_data.data("sides");
+
     let rolls = rollset.find(".roll");
     $.each(rolls, function(key, value) {
       let roll = $(value);
@@ -153,7 +203,7 @@ var save_data = function(e) {
       let modifier = roll.children(".modifier").first().text();
       rollset_data.push({"name": name, "modifier": modifier});
     });
-    data.push({"name": rollset_name, "rolls": rollset_data});
+    data.push({"name": rollset_name, "dice": dice, "sides": sides, "rolls": rollset_data});
   });
   $("#data_loader").val(JSON.stringify(data));
 }
@@ -163,7 +213,7 @@ var load_data = function(e) {
   let data = JSON.parse($("#data_loader").val());
 
   for (const rollset_data of data) {
-    let rollset = _add_rollset(rollset_data["name"]);
+    let rollset = _add_rollset(rollset_data.name, rollset_data.dice, rollset_data.sides);
     for (const roll_data of rollset_data.rolls) {
       let roll = _add_roll_to_rollset(rollset, roll_data.name, roll_data.modifier);
     }
