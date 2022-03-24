c488d480efbd5b77caab25f0d02617ae4e313700
Gabe Leonard <gabe@databaseguy.com>
2022-03-23 23:14:01 -0400

    Adds hp
    Still want a better initiative tracker, but that's more work. This is enough to run a full set of monsters for basic things, I think.

--- multi_roller.js
+++ multi_roller.js
@@ -1,3 +1,18 @@
+/*
+TODO:
+* Put numbers on each card to uniquely identify them.
+* Preview/highlight highest initiative actor in each block.
+* Enable stepping through initiative.
+* Add Player option to track skills.
+* Add ability to give monsters ability checkboxes.
+* Add ability to give monsters HP.
+* Confirm on re-rolling initiative.
+* Fix saving.
+* Add customizable player initiative slots?
+* Sort rows by initiative?
+* Allow cookie saving of monsters?
+*/
+
 class Roll {
   constructor(num, sides) {
     this.num = num;
@@ -16,6 +31,24 @@ class Roll {
     return new RollResult(total, rolled_values, this._isMax(total, modifier), this._isMin(total, modifier));
   }
 
+  simpleRoll(modifier=0) {
+    let total = 0;
+    for (let i = 0; i < this.num; i++) {
+      let value = Math.ceil(Math.random() * this.sides);
+      total += value;
+    }
+    return total + modifier;
+  }
+
+
+  static rollndn(num, sides, modifier=0) {
+    let total = 0;
+    for (let i = 0; i < num; i++) {
+      let value = Math.ceil(Math.random() * sides);
+      total += value;
+    }
+    return total + modifier;
+  }
   max() {
     return this.num * this.sides;
   }
@@ -43,25 +76,43 @@ class RollResult {
 }
 
 class RollEntry {
-  constructor(name, modifier, damage_modifier) {
+  constructor(name, modifier, damage_modifier, health_modifier, health) {
     this.name = name;
     this.modifier = modifier;
     this.damage_modifier = damage_modifier;
+    this.health_modifier = health_modifier;
+    this.health = health;
   }
 }
 
+// TODO: This is quickly just becoming a monster list, and should be renamed as such.
 class Rollset {
-  constructor(name, roll, initiative_bonus=0, damage_roll=null) {
+  constructor(name, roll, initiative_bonus=0, damage_roll=null, health_roll=null) {
     this.name = name;
     this.roll = roll;
     this.initiative_bonus = initiative_bonus;
-    this.initiative = null;
     this.damage_roll = damage_roll;
+    this.health_roll = health_roll;
+    this.initiative = null;
+    this.health = null;
     this.entries = []
   }
 
-  addEntry(name, modifier, damage_modifier=null) {
-    this.entries.push(new RollEntry(name, modifier, damage_modifier));
+  addEntry(name, modifier, damage_modifier=null, health_modifier=null) {
+    let health = 0;
+    if (this.hasDamage()) {
+      health = this.health_roll.simpleRoll(health_modifier);
+    }
+    let new_entry = new RollEntry(name, modifier, damage_modifier, health_modifier, health);
+    this.entries.push(new_entry);
+    return new_entry;
+  }
+
+  getEntry(index=null) {
+    if (index === null) {
+      index = this.entries.length - 1;
+    }
+    return this.entries[index];
   }
 
   removeEntry(index) {
@@ -81,6 +132,17 @@ class Rollset {
 
   hasDamage() {
     return this.damage_roll !== null;
+  }
+
+  getHealthRollResults() {
+    if (!this.hasHealth()) {
+      return null;
+    }
+    return this._getRollResults(this.health_roll, this.entries.map(x => x.health_modifier));
+  }
+
+  hasHealth() {
+    return this.health_roll !== null;
   }
 
   numEntries() {
@@ -113,7 +175,7 @@ $(document).ready(function() {
   _add_roll_to_rollset(rollset, "Goblin Warrior", 10);
   _add_roll_to_rollset(rollset, "Goblin Wizard", -10);
 
-  let rollset2 = _add_rollset("attack", "Goblin Attacks w/ Damage", 1, 20, 3, 2, 8);
+  let rollset2 = _add_rollset("attack", "Goblin Attacks w/ Damage", 1, 20, 3, 2, 8, 2, 10);
   _add_roll_to_rollset(rollset2, "Goblin Warrior", 3, 5);
   _add_roll_to_rollset(rollset2, "Goblin Wizard", -2, -3);
 
@@ -127,6 +189,8 @@ let add_rollset = function(e) {
   let rollset_sides_input = $("#rollset_builder_sides");
   let rollset_damage_dice_input = $("#rollset_builder_damage_dice");
   let rollset_damage_sides_input = $("#rollset_builder_damage_sides");
+  let rollset_health_dice_input = $("#rollset_builder_health_dice");
+  let rollset_health_sides_input = $("#rollset_builder_health_sides");
   let rollset_initiative_bonus_input = $("#rollset_builder_initiative_bonus");
   let type = rollset_type_input.val();
   let name = rollset_name_input.val();
@@ -134,8 +198,9 @@ let add_rollset = function(e) {
   let sides = rollset_sides_input.val();
   let damage_dice = rollset_damage_dice_input.val();
   let damage_sides = rollset_damage_sides_input.val();
+  let health_dice = rollset_health_dice_input.val();
+  let health_sides = rollset_health_sides_input.val();
   let initiative_bonus = rollset_initiative_bonus_input.val();
-  console.log(initiative_bonus)
   rollset_name_input.val("");
   rollset_dice_input.val("");
   rollset_sides_input.val("");
@@ -158,12 +223,18 @@ let add_rollset = function(e) {
   if (!damage_sides) {
     damage_sides = 8;
   }
-  _add_rollset(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides);
+  if (!health_dice) {
+    damage_dice = 1;
+  }
+  if (!health_sides) {
+    damage_sides = 8;
+  }
+  _add_rollset(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides, health_dice, health_sides);
 };
 
-let _add_rollset = function(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides) {
+let _add_rollset = function(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides, health_dice, health_sides) {
   let rollset_index = self.rollsets.length;
-  let rollset = _add_rollset_obj(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides);
+  let rollset = _add_rollset_obj(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides, health_dice, health_sides);
   return _add_rollset_dom(rollset, rollset_index);
 }
 
@@ -173,12 +244,15 @@ var check_enter_rollset = function(event) {
     }
 }
 
-let _add_rollset_obj = function(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides) {
+// TODO: Consider mergine with parent function. This is very short for how many
+// variables it makes us pass around.
+let _add_rollset_obj = function(type, name, dice, sides, initiative_bonus, damage_dice, damage_sides, health_dice, health_sides) {
   let roll = new Roll(dice, sides);
   let rollgroup = null;
   if (type === "attack") {
     let damage_roll = new Roll(damage_dice, damage_sides);
-    rollset = new Rollset(name, roll, initiative_bonus, damage_roll);
+    let health_roll = new Roll(health_dice, health_sides);
+    rollset = new Rollset(name, roll, initiative_bonus, damage_roll, health_roll);
   } else {
     rollset = new Rollset(name, roll);
   }
@@ -200,15 +274,19 @@ let _add_rollset_dom = function(rollset, rollset_index) {
 
   let roll_summary = newrollset.find(".roll_summary").first();
   let damage_summary = newrollset.find(".damage_summary").first();
+  let health_summary = newrollset.find(".health_summary").first();
 
   let roll_summary_text = `Roll: ${rollset.roll.num}d${rollset.roll.sides}`;
   roll_summary.text(roll_summary_text);
   if (rollset.hasDamage()) {
-    damage_summary_text = `Dmg:  ${rollset.damage_roll.num}d${rollset.damage_roll.sides}`;
+    let damage_summary_text = `Dmg:  ${rollset.damage_roll.num}d${rollset.damage_roll.sides}`;
     damage_summary.text(damage_summary_text);
+    let health_summary_text = `HP:  ${rollset.health_roll.num}d${rollset.health_roll.sides}`;
+    health_summary.text(health_summary_text);
   } else {
     damage_summary.css("display", "none");
-    newrollset.find(".damage_modifier").css("display", "none");
+    newrollset.find(".damage_modifier_input").css("display", "none");
+    newrollset.find(".health_modifier_input").css("display", "none");
   }
 
   let roll_placeholder = _get_roll_placeholder(rollset.hasDamage());
@@ -233,11 +311,63 @@ let _add_rollset_dom = function(rollset, rollset_index) {
   return newrollset;
 };
 
+var add_roll_to_rollset = function(e) {
+  let rollset_dom = $(event.target).parents(".rollset");
+  let roll_name = rollset_dom.find(".roll_name_input").first().val();
+  let roll_modifier = rollset_dom.find(".roll_modifier_input").first().val();
+  let damage_modifier = rollset_dom.find(".damage_modifier_input").first().val();
+  let health_modifier = rollset_dom.find(".health_modifier_input").first().val();
+
+  _add_roll_to_rollset(rollset_dom, roll_name, roll_modifier, damage_modifier, health_modifier);
+}
+
+var _add_roll_to_rollset = function(rollset_dom, name, roll_modifier, damage_modifier='', health_modifier='') {
+  roll_modifier = Number(roll_modifier);
+  damage_modifier = Number(damage_modifier);
+  health_modifier = Number(health_modifier);
+
+  let index = Number(rollset_dom.data("index"));
+  let rollset = rollsets[index];
+
+  if (!name) {
+    name = "#" + String(rollset.numEntries() + 1);
+  }
+
+  let entry = rollset.addEntry(name, roll_modifier, damage_modifier, health_modifier);
+
+  rollset_dom.find(".placeholder").remove();
+
+  let newroll = _get_roll(rollset.hasDamage());
+  newroll.removeAttr("id");
+  // -1 because we already added this entry.
+  newroll.attr("data-index", rollset.numEntries() - 1);
+  newroll.find(".roll_name").first().text(name);
+
+  let maybe_plus = roll_modifier >= 0 ? "+" : "";
+  roll_modifier = maybe_plus + String(roll_modifier);
+  newroll.find(".roll_modifier").text(String(roll_modifier));
+
+  if (rollset.hasDamage()) {
+    maybe_plus = damage_modifier >= 0 ? "+" : "";
+    damage_modifier = maybe_plus + String(damage_modifier);
+    newroll.find(".damage_modifier").text(String(damage_modifier));
+    newroll.find(".roll_health_row").text(String(entry.health));
+  }
+
+  rollset_dom.find(".rollset_rolls").first().append(newroll);
+
+  $("#rollsets .remove_roll").off("click");
+  $("#rollsets .remove_roll").click(remove_roll);
+
+  return newroll;
+}
+
 var _get_roll = function(has_damage) {
   let roll = $("#roll_template").clone();
   roll.removeAttr("id");
   if (!has_damage) {
     roll.find(".roll_damage_section").first().css("display", "none");
+    roll.find(".roll_health_row").first().css("display", "none");
   }
   return roll;
 }
@@ -277,6 +407,20 @@ var rollInitiative = function(e) {
     initiative_dom.text(`[${initiative}]`)
     initiative_dom.css("visibility", "visible");
   });
+}
+
+var change_rollset_builder_type = function() {
+  let dropdown = $("#rollset_builder_type").first();
+  let element = $("#attack_values");
+  if (dropdown.val() === "attack") {
+    element.css("display", "block");
+  } else if (dropdown.val() === "roll") {
+    element.css("display", "none");
+    element.find("#rollset_builder_damage_dice").first().val('');
+    element.find("#rollset_builder_damage_sides").first().val('');
+    element.find("#rollset_builder_initiative_bonus").first().val('');
+  }
+
 }
 
 // var forEachRollsetEntry = function(cb) {
@@ -377,55 +521,6 @@ var remove_rollset = function(e) {
       element.attr("data-index", idx - 1);
     }
   });
-}
-
-var add_roll_to_rollset = function(e) {
-  let rollset_dom = $(event.target).parents(".rollset");
-  let roll_name = rollset_dom.find(".roll_name_input").first().val();
-  let roll_modifier = rollset_dom.find(".roll_modifier_input").first().val();
-  let damage_modifier = rollset_dom.find("damage_modifier_input").first().val();
-  console.log(`${roll_name}, ${roll_modifier}, ${damage_modifier}`)
-
-  _add_roll_to_rollset(rollset_dom, roll_name, roll_modifier, damage_modifier);
-}
-
-var _add_roll_to_rollset = function(rollset_dom, name, roll_modifier, damage_modifier='') {
-  roll_modifier = Number(roll_modifier);
-  damage_modifier = Number(damage_modifier);
-
-  let index = Number(rollset_dom.data("index"));
-  let rollset = rollsets[index];
-
-  if (!name) {
-    name = "#" + String(rollset.numEntries() + 1);
-  }
-
-  rollset.addEntry(name, roll_modifier, damage_modifier);
-
-  rollset_dom.find(".placeholder").remove();
-
-  let newroll = _get_roll(rollset.hasDamage());
-  newroll.removeAttr("id");
-  // -1 because we already added this entry.
-  newroll.attr("data-index", rollset.numEntries() - 1);
-  newroll.find(".roll_name").first().text(name);
-
-  let maybe_plus = roll_modifier >= 0 ? "+" : "";
-  roll_modifier = maybe_plus + String(roll_modifier);
-  newroll.find(".roll_modifier").text(String(roll_modifier));
-
-  if (rollset.hasDamage()) {
-    maybe_plus = damage_modifier >= 0 ? "+" : "";
-    damage_modifier = maybe_plus + String(damage_modifier);
-    newroll.find(".damage_modifier").text(String(damage_modifier));
-  }
-
-  rollset_dom.find(".rollset_rolls").first().append(newroll);
-
-  $("#rollsets .remove_roll").off("click");
-  $("#rollsets .remove_roll").click(remove_roll);
-
-  return newroll;
 }
 
 var save_data = function(e) {
